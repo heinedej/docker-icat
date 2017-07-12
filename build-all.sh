88e4d90291d1fd448957c8147b2a9d8e079f2bca
root <root@srvubuiro001.uct.ac.za>
2017-07-12 08:02:53 +0200

    Changed to heinedej namespace

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-docker build -t mercury/icat:3.3.1 3/3.3.1
+docker build -t heinedej/icat:3.3.1 3/3.3.1
 
-docker build -t mercury/icat:4-base 4/base
+docker build -t heinedej/icat:4-base 4/base
 
-docker build -t mercury/icat:4.1-base 4/4.1-base
-docker build -t mercury/icat:4.1.8 4/4.1.8
-docker build -t mercury/icat:4.1.9 4/4.1.9
-docker build -t mercury/icat:4.1.10 4/4.1.10
+docker build -t heinedej/icat:4.1-base 4/4.1-base
+docker build -t heinedej/icat:4.1.8 4/4.1.8
+docker build -t heinedej/icat:4.1.9 4/4.1.9
+docker build -t heinedej/icat:4.1.10 4/4.1.10
 
-docker build -t mercury/icat:4.2-base 4/4.2-base
-docker build -t mercury/icat:4.2.0 4/4.2.0
+docker build -t heinedej/icat:4.2-base 4/4.2-base
+docker build -t heinedej/icat:4.2.0 4/4.2.0
